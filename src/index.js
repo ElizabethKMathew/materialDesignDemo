5436f67a5c343d9c99462776f7ef096f81dc4cc3
elizabeth <elizabeth@qburst.com>
2018-03-27 18:13:44 +0530

    webpack configuration

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import 'bootstrap';
+require('./js/mdb.min');
+
+import './css/mdb.css';
+import './scss/index.scss';
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    module: {
+      rules: [
+        {
+        test: /\.(scss|css)$/,
+        use: [{
+            loader: 'style-loader' // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader' // translates CSS into CommonJS
+          },
+          {
+            loader: 'sass-loader' // compiles Sass to CSS
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'url-loader',
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      "$": "jquery",
+      "jquery": "jquery",
+      "jQuery": "jquery",
+      "window.jQuery": "jquery"
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+};
